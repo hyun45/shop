cd9eb8a9b9829586e5d949b50bc2d42763118e4c
45183 <hsb9743@naver.com>
2024-02-26 13:36:43 +0900

    onDelete CASCADE 설정

--- server/models/cartItem.js
+++ server/models/cartItem.js
@@ -26,7 +26,7 @@ module.exports = class CartItem extends Sequelize.Model {
     }
 
     static associate(db){
-        db.CartItem.belongsTo(db.User,  { foreignKey: 'userId', targetKey: 'userId', onDelete: 'CASCADE' });
+        db.CartItem.belongsTo(db.User,  { foreignKey: 'userId', targetKey: 'userId', onDelete: 'CASCADE' }); // 회원 탈퇴 시 장바구니 내역도 같이 삭제
         db.CartItem.belongsTo(db.Product,  { foreignKey: 'productId', targetKey: 'productId' });
         // db.CartItem.belongsTo(db.Cart,  { foreignKey: 'cartId', targetKey: 'cartId' });
     }
